ce5c942384652bd364ceaf0d95078a242d0c8547
rainyuxia0112 <44214595+rainyuxia0112@users.noreply.github.com>
2019-03-29 22:22:41 -0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,12 +1,20 @@
+""" this py is to build a simple function to cope with missing data in dataframe
+"""
+### import packages and data
 import pandas as pd
 data=pd.read_csv('/Users/rain/Desktop/data.csv',header= None,sep=',',index_col=None)
 X=data.iloc[:,0:7].values
 Y=data.iloc[:,8:9].values
 
-
 #missing data
-def missing_data(n,arr):
-    '''n is which col as missing col, using randomforst regressor'''
+def missing_data(n,arr,type):
+    '''
+    this function is to use random forest model to cope with missing data
+    n -- col as missing col, using randomforst regressor
+    arr -- the predictors (X)
+    type -- the type of missing data, such as continuous or category
+    return -- new array of predictors
+    '''
     import numpy as np
     import pandas as pd
     index = np.array(np.where(arr[:, n] == 0))[0]
@@ -24,71 +32,6 @@ def missing_data(n,arr):
         arr[index[i], n] = y_pre[i]
     a=arr
     return (a)
-
-
-from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-X_ = sc_x.fit_transform(X)
-sc_y = StandardScaler()
-Y_ = sc_y.fit_transform(Y.reshape(-1,1))
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X_, Y, test_size = 0.2, random_state = 0)
-
-# SVR
-from sklearn.svm import SVR
-reg=SVR(kernel='rbf')
-reg.fit(X_train,y_train)
-y_pre=reg.predict(X_test)
-
-import sklearn
-sklearn.metrics.mean_squared_error(y_true=y_test, y_pred=y_pre)
-
-
-#poly
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
-
-from sklearn.preprocessing import PolynomialFeatures
-ploy_reg= PolynomialFeatures(degree=1)
-X_ploy=ploy_reg.fit_transform(X_train)
-from sklearn.linear_model import LinearRegression
-reg=LinearRegression()
-reg.fit(X_ploy,y_train)
-y_pre=reg.predict(ploy_reg.fit_transform(X_test))    #预测时要进行转变
-import sklearn
-sklearn.metrics.mean_squared_error(y_true=y_test, y_pred=y_pre)
-
-
-
-#linear
-
-import numpy as np
-X = np.append(arr=np.ones((103,1)).astype(int), values=X, axis=1)   # 把b0 也变成 b0*x0
-
-import statsmodels.formula.api as sm
-def backwardElimination(x, sl):
-    numVars = len(x[0])
-    for i in range(0, numVars):
-        regressor_OLS = sm.OLS(Y, x).fit()
-        maxVar = max(regressor_OLS.pvalues).astype(float)
-        if maxVar > sl:
-            for j in range(0, numVars - i):
-                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
-                    x = np.delete(x, j, 1)
-    regressor_OLS.summary()
-    return x
-
-from sklearn.model_selection import train_test_split
-#x为数据集的feature熟悉，y为label.
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3)
-
-from sklearn.linear_model import LinearRegression
-reg=LinearRegression()
-reg.fit(x_train,y_train)
-y_pre=reg.predict(x_test)
-import sklearn
-sklearn.metrics.mean_squared_error(y_true=y_test, y_pred=y_pre)
 
 
 
